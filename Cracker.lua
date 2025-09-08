a59fb95badf8b6acb3a232ee718d60ade950acb0
Easy lua <staskuzmin1406@gmail.com>
2025-09-08 23:03:50 +0300

    Update Cracker.lua

--- Cracker.lua
+++ Cracker.lua
@@ -1,5 +1,2 @@
 local utils = loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/AternosBypass/refs/heads/main/utils/Init.lua"))()
-local bypas = "workersdev"
-local bypasser = utils.bypasser()
-
-bypasser(bypas)
+utils.bypasser("workersdev")
