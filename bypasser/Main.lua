789eb6ea3714255d00892292dc0ecd4683dc3b73
Easy lua <staskuzmin1406@gmail.com>
2025-09-08 23:00:49 +0300

    Update Main.lua

--- bypasser/Main.lua
+++ bypasser/Main.lua
@@ -1,1 +1,9 @@
-
+return function(keysystem)
+    if keysystem == "workersdev" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/AternosBypass/main/bypasser/Items/WorkersDev.lua"))()
+    elseif keysystem == "workink" then
+        loadstring(game:HttpGet(""))()
+    elseif keysystem == "platoboost" then
+        loadstring(game:HttpGet(""))()
+    end
+end
