3e6bf032a6ac9c7902261a01156bdb4a08c8f0ff
Easy lua <staskuzmin1406@gmail.com>
2025-09-08 22:42:32 +0300

    Update Init.lua

--- utils/Init.lua
+++ utils/Init.lua
@@ -1,1 +1,24 @@
+local function req(url)
+    return loadstring(game:HttpGet(url))()
+end
+local items = {
+  Disabler = "",
+  randomUtils = "https://raw.githubusercontent.com/Player01010111Cheater/AternosBypass/refs/heads/main/utils/randomUtils.lua",
+  urls = ""
+}
 
+local utils = {}
+
+for name, item in pairs(items) do
+    if type(item) == "string" and item:match("^https?://") then
+        utils[name] = function(...)
+            local f = req(item)
+            utils[name] = f
+            return f(...)
+        end
+    else
+        utils[name] = item
+    end
+end
+
+return utils
